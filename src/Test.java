bd1f899c0adab52c95c6f27ff4b9237e02086987
Gary Leutheuser <g.leutheuser@gmail.com>
2017-12-06 10:51:38 -0500

    Implement timing mechanism

--- src/Test.java
+++ src/Test.java
@@ -36,6 +36,7 @@ public class Test {
         }
         
         // Preprocessing.
+        long startTime = System.nanoTime();
         BWT         L   = new BWT(text);
         SuffixArray SA  = new SuffixArray(text);
         WaveletTree WT  = new WaveletTree(L.getBWT());
@@ -45,6 +46,8 @@ public class Test {
         for (int i = 0; i < text.length(); i++) {
             textAlphabet.add(text.charAt(i));
         }
+        
+        System.out.println("The size of the alphabet of the text is " + textAlphabet.size());
         
         // Compute the C mapping.
         Hashtable<Character, Integer> C = new Hashtable<>();
@@ -61,6 +64,11 @@ public class Test {
             }
         }
         
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+        
+        System.out.println("Construction time: " + duration/1000000);
+        
         // Search for patterns forever.
         Scanner input = new Scanner(System.in);
         String pattern = null;
@@ -69,24 +77,39 @@ public class Test {
             System.out.println("Enter a pattern:");
             pattern = input.nextLine();
 
-            // All values are prepared for the search.
-            int i = pattern.length();
-            int sp = 1;
-            int ep = L.getBWT().length();
-            while (sp <= ep && i >= 1) {
-                char c = pattern.charAt(i-1);
-                sp = C.get(c) + WT.occ(WT.getRoot(), sp - 1, c) + 1;
-                ep = C.get(c) + WT.occ(WT.getRoot(), ep, c);
-                i--;
+            // Run the algorithm a hundred times to get a reasonable time value.
+            startTime = System.nanoTime();
+            int sp = 0;
+            int ep = 0;
+            for (int repeat = 0; repeat < 1000; repeat++) {
+                // All values are prepared for the search.
+                int i = pattern.length();
+                sp = 1;
+                ep = L.getBWT().length();
+                while (sp <= ep && i >= 1) {
+                    char c = pattern.charAt(i-1);
+                    sp = C.get(c) + WT.occ(WT.getRoot(), sp - 1, c) + 1;
+                    ep = C.get(c) + WT.occ(WT.getRoot(), ep, c);
+                    i--;
+                }
             }
+            
+            endTime = System.nanoTime();
+            duration = (endTime - startTime);
+            
             if (ep < sp)
                 System.out.println("Pattern not found.");
             else
                 System.out.println("<sp, ep> pair is <" + sp + ", " + ep + ">");
 
-            for (int j = sp; j <= ep; j++) {
-                System.out.println(SA.getSuffixArray().get(j-1).toString());
-            }
+//            for (int j = sp; j <= ep; j++) {
+//                System.out.println(SA.getSuffixArray().get(j-1).toString());
+//            }
+            
+            System.out.println();
+            System.out.println("The text length is " + text.length());
+            System.out.println("The pattern length is " + pattern.length());
+            System.out.println("1000 repetitions of the search took " + duration/1000000 + " milliseconds.");
         }
     }
 }
--- src/WaveletTree.java
+++ src/WaveletTree.java
@@ -32,31 +32,11 @@ public class WaveletTree {
         
         // The character is in the right child.
         if (node.rightChild.getString().contains(Character.toString(c))) {
-            if (node.rankOne(i) != node.fastRankOne(i)) {
-//                System.out.println("Bit vector: " + Arrays.toString(node.getbitVector()));
-//                System.out.println("Smallrank: " + Arrays.toString(node.smallRank));
-                System.out.println("i: " + i);
-                System.out.println("Block size: " + node.t);
-//                System.out.println("Boundaryrank: " + Arrays.toString(node.boundaryRank));
-                System.out.println("Correct rank = " + node.rankOne(i));
-                System.out.println("Fast rank = " + node.fastRankOne(i));
-                System.out.println();
-            }
             int x = node.fastRankOne(i);
             return occ(node.rightChild, x, c);
         }
         // The character is the left child.
         else {
-            if (node.rankOne(i) != node.fastRankOne(i)) {
-//                System.out.println("Bit vector: " + Arrays.toString(node.getbitVector()));
-//                System.out.println("Smallrank: " + Arrays.toString(node.smallRank));
-                System.out.println("i: " + i);
-                System.out.println("Block size: " + node.t);
-//                System.out.println("Boundaryrank: " + Arrays.toString(node.boundaryRank));
-                System.out.println("Correct rank = " + node.rankOne(i));
-                System.out.println("Fast rank = " + node.fastRankOne(i));
-                System.out.println();
-            }
             int x = i - node.fastRankOne(i); 
             return occ(node.leftChild, x, c);
         }
